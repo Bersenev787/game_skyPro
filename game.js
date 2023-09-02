37620e00c94535e47471dbb69748b4dc41adc81a
Maksim Bersenev <106951162+Bersenev787@users.noreply.github.com>
2023-09-02 19:28:18 +0300

    hw_3.1

--- game.js
+++ game.js
@@ -1,40 +1,88 @@
 // game.js
-let minutes = 0;
-let seconds = 0;
 
-function updateTimer() {
-  seconds++;
+// Получаем уровень сложности из localStorage
+const selectedLevel = localStorage.getItem("selectedLevel");
+const numPairs = parseInt(selectedLevel) * 6; // Количество пар карт в зависимости от уровня
 
-  if (seconds === 60) {
-    minutes++;
-    seconds = 0;
+// Генерация пар карт
+const suits = ["пики", "черви", "бубны", "крести"];
+const ranks = ["туз", "король", "дама", "валет", "10", "9", "8", "7", "6"];
+
+const generateCardPairs = (numPairs) => {
+  const cardPairs = [];
+
+  while (cardPairs.length < numPairs) {
+    const suit = suits[Math.floor(Math.random() * suits.length)];
+    const rank = ranks[Math.floor(Math.random() * ranks.length)];
+    const image = `/img/${rank}_${suit}.png`;
+
+    if (!cardPairs.includes(image)) {
+      cardPairs.push(image);
+      cardPairs.push(image);
+    }
   }
 
-  document.getElementById("minutes").textContent = formatTime(minutes);
-  document.getElementById("seconds").textContent = formatTime(seconds);
+  return cardPairs;
+};
+
+const timerElement = document.querySelector(".start_first_timer");
+let timerInterval; // Переменная для хранения интервала таймера
+let seconds = 0;
+let minutes = 0;
+
+// Функция для обновления таймера
+function updateTimer() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+  }
+
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  timerElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
+}
+function startTimer(restart = false) {
+  if (restart) {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    return;
+  }
+
+  seconds = 0;
+  minutes = 0;
+  timerInterval = setInterval(updateTimer, 1000);
+  updateTimer();
 }
 
-function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
-}
+// пока так
+// Отложенный старт после показа карт
+setTimeout(startTimer, 5000);
 
-setInterval(updateTimer, 1000);
-
-// JavaScript код для переворачивания карт
+// Подготовка игрового поля
 const cards = document.querySelectorAll(".start_second_img");
+const cardPairs = generateCardPairs(numPairs);
 
-cards.forEach((card) => {
-  card.addEventListener("click", () => {
-    const isFlipped = card.getAttribute("data-flipped");
-    if (isFlipped === "false") {
-      const image = card.getAttribute("data-image");
-      card.src = image;
-      card.classList.add("flipped");
-      card.setAttribute("data-flipped", "true");
-    } else {
-      card.src = "/img/рубашка.png";
-      card.classList.remove("flipped");
-      card.setAttribute("data-flipped", "false");
-    }
-  });
+cards.forEach((card, index) => {
+  // показываем карты перед началом игры
+  card.setAttribute("data-image", cardPairs[index]);
+  card.src = cardPairs[index];
+
+  // скрываем карты через 5 секунд
+  setTimeout(() => {
+    card.setAttribute("data-flipped", false);
+    card.src = "/img/рубашка.png";
+  }, 5000);
 });
+
+// Начать заново
+const startNewGame = document.querySelector(".start_first_btn");
+startNewGame?.addEventListener("click", () => {
+  localStorage.removeItem("selectedLevel");
+  clearInterval(timerInterval);
+  timerInterval = null;
+  window.location.pathname = "/";
+});
+
+export { startTimer, cardPairs };
--- script.js
+++ script.js
@@ -1,42 +1,128 @@
 //script.js
 
-let selectedDifficulty = null;
+import { startTimer, cardPairs } from "./game.js";
 
-const difficultyButtons = document.querySelectorAll(".content_btn_item");
-difficultyButtons.forEach((button) => {
+// Получаем все элементы с классом "start_second_img"
+const cards = document.querySelectorAll(".start_second_img");
+
+let flippedCards = []; // Массив для хранения перевернутых карточек
+let matchedPairs = 0; // Количество пар, которые были найдены
+const container = document.querySelector(".container");
+
+// Функция для переворота карточки
+function flipCard(card) {
+  card.setAttribute("src", card.dataset.image);
+  card.setAttribute("data-flipped", "true");
+}
+
+// Функция для скрытия карточек
+function unflipCards() {
+  cards.forEach((card) => {
+    card.setAttribute("src", "/img/рубашка.png");
+    card.setAttribute("data-flipped", "false");
+  });
+  flippedCards = [];
+}
+
+function showWinner({ winner }) {
+  const time = document.querySelector(".start_first_timer").textContent;
+  const alertPopup = document.createElement("div");
+  alertPopup.classList.add("alert");
+
+  const alertPopupWrapper = document.createElement("div");
+
+  alertPopupWrapper.classList.add("alert-wrapper");
+  alertPopupWrapper.innerHTML = `
+          <img src="img/${winner ? "celebration" : "dead"}.png" alt="" />
+          <h3 class="alert-wrapper-title">Вы ${
+            winner ? "выиграли" : "проиграли"
+          } !</h3>
+          <p class="alert-wrapper-text">Затраченное время:</p>
+          <div class="alert-wrapper-time">
+            ${time}
+          </div>
+          <button class="alert-wrapper-button">Играть снова</button>
+        `;
+
+  container.appendChild(alertPopup);
+  container.appendChild(alertPopupWrapper);
+
+  const restartGame = document.querySelector(".alert-wrapper-button");
+
+  restartGame.addEventListener("click", () => {
+    container.removeChild(alertPopup);
+    container.removeChild(alertPopupWrapper);
+    unflipCards();
+    startTimer();
+  });
+}
+
+// Функция для обработки клика на карточку
+function handleCardClick(event) {
+  const card = event.target;
+
+  if (card.dataset.flipped === "false" && flippedCards.length < 2) {
+    flipCard(card);
+    flippedCards.push(card);
+
+    if (flippedCards.length === 2) {
+      if (flippedCards[0].dataset.image === flippedCards[1].dataset.image) {
+        setTimeout(() => {
+          // Карты совпали
+          matchedPairs++;
+          flippedCards = [];
+
+          // длинна массива сгенерированных карт
+          if (matchedPairs === cardPairs.length / 2) {
+            startTimer(true);
+            showWinner({ winner: true });
+          }
+        }, 1000); // Задержка перед проверкой
+      } else {
+        // Карты не совпали
+        startTimer(true);
+        showWinner({ winner: false });
+      }
+    }
+  }
+}
+
+// Добавляем обработчик клика на каждую карточку
+cards.forEach((card) => {
+  card.addEventListener("click", handleCardClick);
+});
+
+// Добавляем обработчик клика на кнопки выбора уровня сложности
+const levelButtons = document.querySelectorAll(".content_btn_item");
+let selectedButton = null;
+levelButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    // Убираем стиль у всех кнопок
-    difficultyButtons.forEach((btn) => {
+    const selectedLevel = button.getAttribute("data-level");
+
+    // Удаляем класс "selected" у всех кнопок
+    levelButtons.forEach((btn) => {
       btn.classList.remove("selected");
     });
 
-    // Добавляем стиль к выбранной кнопке
+    // Добавляем класс "selected" только к выбранной кнопке
     button.classList.add("selected");
 
-    selectedDifficulty = button.textContent;
-    console.log(`Выбран уровень сложности: ${selectedDifficulty}`);
+    // Обновляем выбранную кнопку
+    selectedButton = button;
+
+    localStorage.setItem("selectedLevel", selectedLevel); // Сохраняем выбранный уровень в localStorage
   });
 });
 
+// Добавляем обработчик клика на кнопку "Старт"
 const startButton = document.querySelector(".content_start");
 startButton.addEventListener("click", () => {
-  if (selectedDifficulty !== null) {
-    navigateToGameScreen(selectedDifficulty);
-    window.location.href = "start.html";
-  } else {
-    console.log("Выберите уровень сложности");
-  }
-});
+  const selectedLevel = localStorage.getItem("selectedLevel");
 
-function navigateToGameScreen(difficulty) {
-  console.log(`Переход на игровой экран для уровня сложности ${difficulty}`);
-}
-
-//обработчик кнопки "Старт"
-startButton.addEventListener("click", () => {
-  if (selectedDifficulty !== null) {
-    navigateToGameScreen(selectedDifficulty);
-  } else {
-    console.log("Выберите уровень сложности");
+  if (!selectedLevel) {
+    alert("Пожалуйста, выберите уровень сложности перед началом игры.");
+    return;
   }
+
+  window.location.href = `/start.html`; // Переход на страницу start.html после выбора уровня
 });
